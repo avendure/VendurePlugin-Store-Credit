5a62c8dffc68c57a14bb29ccc6010622ac268572
dylviz <dylanvizcarra@gmail.com>
2023-11-17 17:58:59 -0800

    Added timeout to e2e-spec.ts file

--- test/store-credit.e2e-spec.ts
+++ test/store-credit.e2e-spec.ts
@@ -1,8 +1,8 @@
 import {
-  SqljsInitializer,
-  registerInitializer,
-  createTestEnvironment,
-  testConfig,
+	SqljsInitializer,
+	registerInitializer,
+	createTestEnvironment,
+	testConfig,
 } from "@vendure/testing";
 import { it, describe, afterAll, expect, beforeAll } from "vitest";
 import { StoreCreditPlugin } from "../src/index";
@@ -10,266 +10,266 @@ import { mergeConfig } from "@vendure/core";
 import path from "path";
 import { initialData } from "./fixtures/initial-data";
 import {
-  GetCustomerListDocument,
-  GetCustomerListQuery,
-  CreateStoreCreditDocument,
-  CreateSellerDocument,
-  TransferFromSellerToCustomerDocument,
-  CreateChannelDocument,
-  LanguageCode,
-  CurrencyCode,
-  AssignProductToChannelDocument,
-  GetSellerDocument,
+	GetCustomerListDocument,
+	GetCustomerListQuery,
+	CreateStoreCreditDocument,
+	CreateSellerDocument,
+	TransferFromSellerToCustomerDocument,
+	CreateChannelDocument,
+	LanguageCode,
+	CurrencyCode,
+	AssignProductToChannelDocument,
+	GetSellerDocument,
 } from "./graphql/generated-admin-types";
 import {
-  AddProductToOrderDocument,
-  ClaimCreditDocument,
-  SetShippingAddressDocument,
-  SetBillingAddressDocument,
-  SetShippingMethodDocument,
-  TransitionToStateDocument,
-  AddPaymentToOrderDocument,
+	AddProductToOrderDocument,
+	ClaimCreditDocument,
+	SetShippingAddressDocument,
+	SetBillingAddressDocument,
+	SetShippingMethodDocument,
+	TransitionToStateDocument,
+	AddPaymentToOrderDocument,
 } from "./graphql/generated-shop-types";
 
 registerInitializer("sqljs", new SqljsInitializer("__data__"));
 
 describe("store-credits plugin", () => {
-  const devConfig = mergeConfig(testConfig, {
-    plugins: [StoreCreditPlugin],
-  });
-  const { server, adminClient, shopClient } = createTestEnvironment(devConfig);
-  let started = false;
-  let customers: GetCustomerListQuery["customers"]["items"] = [];
-  let sellerId: string = "";
-  let channelId: string = "";
+	const devConfig = mergeConfig(testConfig, {
+		plugins: [StoreCreditPlugin],
+	});
+	const { server, adminClient, shopClient } = createTestEnvironment(devConfig);
+	let started = false;
+	let customers: GetCustomerListQuery["customers"]["items"] = [];
+	let sellerId: string = "";
+	let channelId: string = "";
 
-  beforeAll(async () => {
-    await server.init({
-      productsCsvPath: path.join(__dirname, "fixtures/products.csv"),
-      initialData: initialData,
-      customerCount: 2,
-    });
-    await adminClient.asSuperAdmin();
+	beforeAll(async () => {
+		await server.init({
+			productsCsvPath: path.join(__dirname, "fixtures/products.csv"),
+			initialData: initialData,
+			customerCount: 2,
+		});
+		await adminClient.asSuperAdmin();
 
-    customers = await adminClient
-      .query(GetCustomerListDocument, {
-        options: { take: 2 },
-      })
-      .then((c) => c.customers.items);
+		customers = await adminClient
+			.query(GetCustomerListDocument, {
+				options: { take: 2 },
+			})
+			.then((c) => c.customers.items);
 
-    started = true;
-  });
+		started = true;
+	}, 60000);
 
-  afterAll(async () => {
-    await server.destroy();
-  });
+	afterAll(async () => {
+		await server.destroy();
+	});
 
-  it("Should start successfully", () => {
-    expect(started).toEqual(true);
-    expect(customers).toHaveLength(2);
-  });
+	it("Should start successfully", () => {
+		expect(started).toEqual(true);
+		expect(customers).toHaveLength(2);
+	});
 
-  it("Should create new seller", async () => {
-    const user = customers[0].user!;
-    const createSellerResult = await adminClient.query(CreateSellerDocument, {
-      input: { name: "Seller 2", customFields: { userId: user.id } },
-    });
-    expect(createSellerResult.createSeller.id).toBeDefined();
-    expect(createSellerResult.createSeller.customFields?.user).toEqual({
-      id: user.id,
-      identifier: user.identifier,
-    });
-    sellerId = createSellerResult.createSeller.id;
-  });
+	it("Should create new seller", async () => {
+		const user = customers[0].user!;
+		const createSellerResult = await adminClient.query(CreateSellerDocument, {
+			input: { name: "Seller 2", customFields: { userId: user.id } },
+		});
+		expect(createSellerResult.createSeller.id).toBeDefined();
+		expect(createSellerResult.createSeller.customFields?.user).toEqual({
+			id: user.id,
+			identifier: user.identifier,
+		});
+		sellerId = createSellerResult.createSeller.id;
+	});
 
-  // it("Should set seller's user", async () => {
-  //   const user = customers[0].user!;
-  //   const setSellerResult = await adminClient.query(SetSellerUserDocument, {
-  //     input: { id: "T_1", customFields: { userId: user.id } },
-  //   });
-  //   expect(setSellerResult.updateSeller.customFields?.user).toEqual({
-  //     id: user.id,
-  //     identifier: user.identifier,
-  //   });
-  // });
+	// it("Should set seller's user", async () => {
+	//   const user = customers[0].user!;
+	//   const setSellerResult = await adminClient.query(SetSellerUserDocument, {
+	//     input: { id: "T_1", customFields: { userId: user.id } },
+	//   });
+	//   expect(setSellerResult.updateSeller.customFields?.user).toEqual({
+	//     id: user.id,
+	//     identifier: user.identifier,
+	//   });
+	// });
 
-  it("Should create new channel with seller", async () => {
-    const createChannelResult = await adminClient.query(CreateChannelDocument, {
-      input: {
-        code: "seller2-ch",
-        token: "seller2ch",
-        sellerId: sellerId,
-        defaultTaxZoneId: "T_1",
-        availableLanguageCodes: [LanguageCode.en],
-        pricesIncludeTax: false,
-        trackInventory: true,
-        defaultLanguageCode: LanguageCode.en,
-        defaultShippingZoneId: "T_1",
-        defaultCurrencyCode: CurrencyCode.USD,
-        availableCurrencyCodes: [CurrencyCode.USD],
-      },
-    });
-    expect(createChannelResult.createChannel.__typename).toEqual("Channel");
-    if (createChannelResult.createChannel.__typename == "Channel") {
-      expect(createChannelResult.createChannel.code).toBeDefined();
-      channelId = createChannelResult.createChannel.id;
-    }
-  });
+	it("Should create new channel with seller", async () => {
+		const createChannelResult = await adminClient.query(CreateChannelDocument, {
+			input: {
+				code: "seller2-ch",
+				token: "seller2ch",
+				sellerId: sellerId,
+				defaultTaxZoneId: "T_1",
+				availableLanguageCodes: [LanguageCode.en],
+				pricesIncludeTax: false,
+				trackInventory: true,
+				defaultLanguageCode: LanguageCode.en,
+				defaultShippingZoneId: "T_1",
+				defaultCurrencyCode: CurrencyCode.USD,
+				availableCurrencyCodes: [CurrencyCode.USD],
+			},
+		});
+		expect(createChannelResult.createChannel.__typename).toEqual("Channel");
+		if (createChannelResult.createChannel.__typename == "Channel") {
+			expect(createChannelResult.createChannel.code).toBeDefined();
+			channelId = createChannelResult.createChannel.id;
+		}
+	});
 
-  it("Should assign product to channel", async () => {
-    const assignResult = await adminClient.query(
-      AssignProductToChannelDocument,
-      {
-        input: { channelId, productIds: ["T_1"], priceFactor: 1 },
-      },
-    );
-    expect(assignResult.assignProductsToChannel).toHaveLength(1);
-    expect(assignResult.assignProductsToChannel[0].id).toEqual("T_1");
-  });
+	it("Should assign product to channel", async () => {
+		const assignResult = await adminClient.query(
+			AssignProductToChannelDocument,
+			{
+				input: { channelId, productIds: ["T_1"], priceFactor: 1 },
+			}
+		);
+		expect(assignResult.assignProductsToChannel).toHaveLength(1);
+		expect(assignResult.assignProductsToChannel[0].id).toEqual("T_1");
+	});
 
-  it("Should create store credit", async () => {
-    const createResult = await adminClient.query(CreateStoreCreditDocument, {
-      input: {
-        key: "abcdef",
-        value: 10000,
-      },
-    });
+	it("Should create store credit", async () => {
+		const createResult = await adminClient.query(CreateStoreCreditDocument, {
+			input: {
+				key: "abcdef",
+				value: 10000,
+			},
+		});
 
-    expect(createResult.createStoreCredit).toEqual({
-      key: "abcdef",
-      value: 10000,
-      isClaimed: false,
-    });
-  });
+		expect(createResult.createStoreCredit).toEqual({
+			key: "abcdef",
+			value: 10000,
+			isClaimed: false,
+		});
+	});
 
-  it("Should claim store credit", async () => {
-    await shopClient.asUserWithCredentials(customers[1].emailAddress, "test");
-    const result = await shopClient.query(ClaimCreditDocument, {
-      key: "abcdef",
-    });
+	it("Should claim store credit", async () => {
+		await shopClient.asUserWithCredentials(customers[1].emailAddress, "test");
+		const result = await shopClient.query(ClaimCreditDocument, {
+			key: "abcdef",
+		});
 
-    expect(result.claim).toEqual({
-      isClaimed: true,
-      key: "abcdef",
-      value: 10000,
-    });
-  });
+		expect(result.claim).toEqual({
+			isClaimed: true,
+			key: "abcdef",
+			value: 10000,
+		});
+	});
 
-  it("Should fail transfer with empty balance", async () => {
-    expect(async () => {
-      await adminClient.query(TransferFromSellerToCustomerDocument, {
-        value: 1000,
-        sellerId: "T_1",
-      });
-    }).rejects.toThrowError("Insufficient balance");
-  });
+	it("Should fail transfer with empty balance", async () => {
+		expect(async () => {
+			await adminClient.query(TransferFromSellerToCustomerDocument, {
+				value: 1000,
+				sellerId: "T_1",
+			});
+		}).rejects.toThrowError("Insufficient balance");
+	});
 
-  describe("Purchase with store-credit", async () => {
-    beforeAll(async () => {
-      await shopClient.asUserWithCredentials(customers[1].emailAddress, "test");
-    });
+	describe("Purchase with store-credit", async () => {
+		beforeAll(async () => {
+			await shopClient.asUserWithCredentials(customers[1].emailAddress, "test");
+		});
 
-    it("Should place an order", async () => {
-      const addProductResult = await shopClient.query(
-        AddProductToOrderDocument,
-        {
-          productVariantId: "T_1",
-          quantity: 1,
-        },
-      );
-      expect(addProductResult.addItemToOrder.__typename).toEqual("Order");
-      if (addProductResult.addItemToOrder.__typename == "Order") {
-        expect(addProductResult.addItemToOrder.code).toBeDefined();
-      }
-    });
+		it("Should place an order", async () => {
+			const addProductResult = await shopClient.query(
+				AddProductToOrderDocument,
+				{
+					productVariantId: "T_1",
+					quantity: 1,
+				}
+			);
+			expect(addProductResult.addItemToOrder.__typename).toEqual("Order");
+			if (addProductResult.addItemToOrder.__typename == "Order") {
+				expect(addProductResult.addItemToOrder.code).toBeDefined();
+			}
+		});
 
-    it("Should set shipping/billing address, shipping method and transition order", async () => {
-      const shippingAddressResult = await shopClient.query(
-        SetShippingAddressDocument,
-        {
-          input: {
-            countryCode: "GB",
-            streetLine1: "68 Farnborough Rd",
-            city: "Farnborough",
-            postalCode: "GU14 6TH",
-            province: "Hampshire",
-          },
-        },
-      );
-      expect(shippingAddressResult.setOrderShippingAddress.__typename).toEqual(
-        "Order",
-      );
+		it("Should set shipping/billing address, shipping method and transition order", async () => {
+			const shippingAddressResult = await shopClient.query(
+				SetShippingAddressDocument,
+				{
+					input: {
+						countryCode: "GB",
+						streetLine1: "68 Farnborough Rd",
+						city: "Farnborough",
+						postalCode: "GU14 6TH",
+						province: "Hampshire",
+					},
+				}
+			);
+			expect(shippingAddressResult.setOrderShippingAddress.__typename).toEqual(
+				"Order"
+			);
 
-      const billingAddressResult = await shopClient.query(
-        SetBillingAddressDocument,
-        {
-          input: {
-            countryCode: "GB",
-            streetLine1: "68 Farnborough Rd",
-            city: "Farnborough",
-            postalCode: "GU14 6TH",
-            province: "Hampshire",
-          },
-        },
-      );
-      expect(billingAddressResult.setOrderBillingAddress.__typename).toEqual(
-        "Order",
-      );
+			const billingAddressResult = await shopClient.query(
+				SetBillingAddressDocument,
+				{
+					input: {
+						countryCode: "GB",
+						streetLine1: "68 Farnborough Rd",
+						city: "Farnborough",
+						postalCode: "GU14 6TH",
+						province: "Hampshire",
+					},
+				}
+			);
+			expect(billingAddressResult.setOrderBillingAddress.__typename).toEqual(
+				"Order"
+			);
 
-      const shippingMethodResult = await shopClient.query(
-        SetShippingMethodDocument,
-        { ids: "T_1" },
-      );
-      expect(shippingMethodResult.setOrderShippingMethod.__typename).toEqual(
-        "Order",
-      );
+			const shippingMethodResult = await shopClient.query(
+				SetShippingMethodDocument,
+				{ ids: "T_1" }
+			);
+			expect(shippingMethodResult.setOrderShippingMethod.__typename).toEqual(
+				"Order"
+			);
 
-      const transitionResult = await shopClient.query(
-        TransitionToStateDocument,
-        { state: "ArrangingPayment" },
-      );
-      expect(transitionResult.transitionOrderToState?.__typename).toEqual(
-        "Order",
-      );
-    });
+			const transitionResult = await shopClient.query(
+				TransitionToStateDocument,
+				{ state: "ArrangingPayment" }
+			);
+			expect(transitionResult.transitionOrderToState?.__typename).toEqual(
+				"Order"
+			);
+		});
 
-    it("Should add payment", async () => {
-      const addPaymentReuslt = await shopClient.query(
-        AddPaymentToOrderDocument,
-        {
-          input: { method: "store-credit", metadata: {} },
-        },
-      );
-      expect(addPaymentReuslt.addPaymentToOrder.__typename).toEqual("Order");
-      if (addPaymentReuslt.addPaymentToOrder.__typename == "Order") {
-        expect(addPaymentReuslt.addPaymentToOrder.state).toEqual(
-          "PaymentSettled",
-        );
-      }
-    });
+		it("Should add payment", async () => {
+			const addPaymentReuslt = await shopClient.query(
+				AddPaymentToOrderDocument,
+				{
+					input: { method: "store-credit", metadata: {} },
+				}
+			);
+			expect(addPaymentReuslt.addPaymentToOrder.__typename).toEqual("Order");
+			if (addPaymentReuslt.addPaymentToOrder.__typename == "Order") {
+				expect(addPaymentReuslt.addPaymentToOrder.state).toEqual(
+					"PaymentSettled"
+				);
+			}
+		});
 
-    it("Should add credits to seller's account", async () => {
-      const sellerResult = await adminClient.query(GetSellerDocument, {
-        id: sellerId,
-      });
+		it("Should add credits to seller's account", async () => {
+			const sellerResult = await adminClient.query(GetSellerDocument, {
+				id: sellerId,
+			});
 
-      expect(sellerResult.seller?.customFields?.accountBalance).toBeGreaterThan(
-        0,
-      );
-    });
-  });
+			expect(sellerResult.seller?.customFields?.accountBalance).toBeGreaterThan(
+				0
+			);
+		});
+	});
 
-  it("Should transfer balance to customer account", async () => {
-    const transferResult = await adminClient.query(
-      TransferFromSellerToCustomerDocument,
-      {
-        value: 1000,
-        sellerId: sellerId,
-      },
-    );
+	it("Should transfer balance to customer account", async () => {
+		const transferResult = await adminClient.query(
+			TransferFromSellerToCustomerDocument,
+			{
+				value: 1000,
+				sellerId: sellerId,
+			}
+		);
 
-    expect(
-      transferResult.transferCreditfromSellerToCustomer.customerAccountBalance,
-    ).toEqual(1000);
-  });
+		expect(
+			transferResult.transferCreditfromSellerToCustomer.customerAccountBalance
+		).toEqual(1000);
+	});
 });
